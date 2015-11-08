7fc13f20b0c1f76a27694dac9a1e0db205bbd3d6
Sergey Kobets <sergey.kobec@gmail.com>
2015-11-08 19:22:57 +0300

    exp

---
+++ core/src/main/java/pro/iamgamer/core/database/repository/EmbeddedDBModule.java
@@ -0,0 +1,20 @@
+package pro.iamgamer.core.database.repository;
+
+import com.google.inject.AbstractModule;
+import ru.vyarus.guice.persist.orient.OrientModule;
+import ru.vyarus.guice.persist.orient.RepositoryModule;
+import ru.vyarus.guice.persist.orient.db.scheme.SchemeInitializer;
+
+/**
+ * Created by Sergey Kobets on 08.11.2015.
+ */
+public class EmbeddedDBModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+        install(new OrientModule("plocal:test", "admin", "admin")
+                .autoCreateLocalDatabase(true));
+        install(new RepositoryModule());
+        bind(SchemeInitializer.class).to(SchemeInitializerImp.class);
+    }
+}
