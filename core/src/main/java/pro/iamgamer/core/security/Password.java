b303a2bcf711dab175f352702b7ae31504f2054a
Sergey Kobets <sergey.kobec@gmail.com>
2015-11-30 19:54:30 +0300

    pwd hashing

---
+++ core/src/main/java/pro/iamgamer/core/security/Password.java
@@ -0,0 +1,39 @@
+package pro.iamgamer.core.security;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Arrays;
+import java.util.Random;
+
+
+/**
+ * Created by sergey.kobets on 30.11.2015.
+ */
+public final class Password {
+
+    private static final Random RANDOM = new SecureRandom();
+    private static final int ITERATIONS = 10000;
+    private static final int KEY_LENGTH = 512;
+
+    public static byte[] randomSalt() {
+        byte[] salt = new byte[31];
+        RANDOM.nextBytes(salt);
+        return salt;
+    }
+
+    public static byte[] hash(char[] password, byte[] salt) {
+        PBEKeySpec spec = new PBEKeySpec(password, salt, ITERATIONS, KEY_LENGTH);
+        Arrays.fill(password, Character.MIN_VALUE);
+        try {
+            SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+            return skf.generateSecret(spec).getEncoded();
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            throw new RuntimeException(e);
+        } finally {
+            spec.clearPassword();
+        }
+    }
+}
