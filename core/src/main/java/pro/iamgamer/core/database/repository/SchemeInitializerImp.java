7548034930f0c516775c367a469e28b532d75fe0
Sergey Kobets <sergey.kobec@gmail.com>
2015-11-30 20:48:33 +0300

    user dao

--- core/src/main/java/pro/iamgamer/core/database/repository/SchemeInitializerImp.java
+++ core/src/main/java/pro/iamgamer/core/database/repository/SchemeInitializerImp.java
@@ -1,7 +1,9 @@
 package pro.iamgamer.core.database.repository;
 
 import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
+import com.orientechnologies.orient.core.metadata.schema.OSchemaProxy;
 import pro.iamgamer.core.model.Lol;
+import pro.iamgamer.core.model.User;
 import ru.vyarus.guice.persist.orient.db.PersistentContext;
 import ru.vyarus.guice.persist.orient.db.pool.DocumentPool;
 import ru.vyarus.guice.persist.orient.db.scheme.SchemeInitializer;
@@ -18,7 +20,10 @@ public class SchemeInitializerImp implements SchemeInitializer {
 
     @Override
     public void initialize() {
-        //persistentContext.getConnection().getMetadata().getSchema().createClass(Lol.class);
-        System.out.println("lol");
+        final OSchemaProxy schema = persistentContext.getConnection().getMetadata().getSchema();
+        if(!schema.existsClass(User.class.getSimpleName())){
+            schema.createClass(User.class);
+        }
+
     }
 }
--- core/src/main/java/pro/iamgamer/core/database/repository/UserRepository.java
+++ core/src/main/java/pro/iamgamer/core/database/repository/UserRepository.java
@@ -3,6 +3,9 @@ package pro.iamgamer.core.database.repository;
 import com.google.inject.ProvidedBy;
 import com.google.inject.internal.DynamicSingletonProvider;
 import com.google.inject.persist.Transactional;
+import com.orientechnologies.orient.core.id.ORID;
+import com.orientechnologies.orient.core.metadata.schema.OType;
+import com.orientechnologies.orient.core.record.impl.ODocument;
 import pro.iamgamer.core.model.Lol;
 import pro.iamgamer.core.model.User;
 import ru.vyarus.guice.persist.orient.support.repository.mixin.crud.DocumentCrud;
@@ -13,5 +16,11 @@ import ru.vyarus.guice.persist.orient.support.repository.mixin.crud.ObjectCrud;
  */
 @Transactional
 @ProvidedBy(DynamicSingletonProvider.class)
-public interface UserRepository extends DocumentCrud<Lol> {
+public abstract class UserRepository implements DocumentCrud<User> {
+    public ORID insert(User user){
+        final ODocument entries = create();
+        entries.field("loginName", user.getLoginName(), OType.STRING);
+        final ODocument save = save(entries);
+        return save.getIdentity();
+    }
 }
