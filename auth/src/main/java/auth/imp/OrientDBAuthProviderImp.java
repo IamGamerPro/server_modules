e5f6ca80cbe4fdcc0c2792ecedcc350e0ca35359
Sergey Kobets <sergey.kobec@gmail.com>
2016-02-24 20:56:54 +0300

    user fix

--- auth/src/main/java/auth/imp/OrientDBAuthProviderImp.java
+++ auth/src/main/java/auth/imp/OrientDBAuthProviderImp.java
@@ -4,11 +4,15 @@ import auth.OrientDBAuthProvider;
 import client.OrientClient;
 import client.OrientGraphAsync;
 import com.orientechnologies.orient.core.sql.OCommandSQL;
+import com.tinkerpop.blueprints.Vertex;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.auth.User;
+
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 
 /**
@@ -16,7 +20,7 @@ import io.vertx.ext.auth.User;
  */
 public class OrientDBAuthProviderImp implements OrientDBAuthProvider {
     private OrientClient orientClient;
-    private static final OCommandSQL loginQuery = new OCommandSQL("select from AppUser where login = ?");
+    private static final OCommandSQL loginQuery = new OCommandSQL("select from User where login = ?");
 
     public OrientDBAuthProviderImp(OrientClient orientClient) {
         this.orientClient = orientClient;
@@ -38,8 +42,12 @@ public class OrientDBAuthProviderImp implements OrientDBAuthProvider {
             if (graphAsyncAsyncHandler.succeeded()) {
                 OrientGraphAsync orientGraphAsync = graphAsyncAsyncHandler.result();
                 orientGraphAsync.command((orientGraph -> {
-                    Object execute = orientGraph.command(loginQuery).execute(username);
-                    System.out.println(execute);
+                    Iterable<Vertex> result = orientGraph.command(loginQuery).execute(username);
+                    Stream<Vertex> stream = StreamSupport.stream(result.spliterator(), false);
+                    stream.findFirst().map(user -> {
+                        user.getProperty("");
+                        return null;
+                    });
                     return new UserCredentials();
                     /*лучше поменять на стринги от хешей, так будет эффективнее*/
                     /*final byte[] passwordInDb = execute.field("password");
--- auth/src/test/java/AuthTest.java
+++ auth/src/test/java/AuthTest.java
@@ -1,6 +1,7 @@
 import auth.OrientDBAuthProvider;
 import client.OrientClient;
 import com.orientechnologies.orient.core.metadata.schema.OType;
+import com.tinkerpop.blueprints.impls.orient.OrientGraphFactory;
 import com.tinkerpop.blueprints.impls.orient.OrientGraphNoTx;
 import com.tinkerpop.blueprints.impls.orient.OrientVertexType;
 import io.vertx.core.Vertx;
@@ -13,6 +14,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.HashMap;
 import java.util.Map;
 
 
@@ -38,19 +40,21 @@ public class AuthTest {
 
     @BeforeClass
     public static void beforeClass() {
-        /*OrientGraphFactory orientGraphFactory = new OrientGraphFactory("plocal:/test");
+        OrientGraphFactory orientGraphFactory = new OrientGraphFactory("plocal:/test");
         OrientGraphNoTx noTx = orientGraphFactory.getNoTx();
         try {
-            //noTx.dropVertexType("User");
+            if(noTx.getVertexType("User") != null) {
+                noTx.dropVertexType("User");
+            }
             Map<String, OType> fields = new HashMap<>();
             fields.put("login", OType.STRING);
             fields.put("password", OType.STRING);
             fields.put("salt", OType.STRING);
-            createVertexType(noTx, "AppUser", fields);
+            createVertexType(noTx, "User", fields);
         } catch (Exception e) {
             e.printStackTrace();
-        }*/
-        /*orientGraphFactory.close();*/
+        }
+        orientGraphFactory.close();
         vertx = Vertx.vertx();
     }
 
@@ -65,11 +69,9 @@ public class AuthTest {
         Async async = context.async(1);
         authProvider.authenticate(new JsonObject("{\"username\":\"test\", \"password\": \"dfltpwd\"}"), event -> {
             if (event.succeeded()) {
-                System.out.println(1);
                 System.out.println(event.result());
                 async.countDown();
             } else {
-                System.out.println(2);
                 System.out.println(event.cause().getMessage());
                 async.countDown();
             }
