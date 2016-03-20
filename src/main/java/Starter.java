417680256ae56570117b91f2bf93c05b56e427c9
Sergey Kobets <sergey.kobec@gmail.com>
2016-03-20 17:49:57 +0300

    fix

--- src/main/java/Starter.java
+++ src/main/java/Starter.java
@@ -1,7 +1,6 @@
 import io.vertx.core.Vertx;
-import io.vertx.ext.web.Router;
-import services.user.PrivateUserPageTest;
 import services.register.RegisterVerticle;
+import services.user.PrivateUserPageTest;
 
 import java.time.LocalTime;
 
@@ -14,7 +13,6 @@ public class Starter {
 
     public static void main(String[] args) {
         Vertx vertx = Vertx.vertx();
-        Router router = Router.router(vertx);
         vertx.deployVerticle(new RegisterVerticle());
         vertx.deployVerticle(new PrivateUserPageTest());
         System.out.println(LocalTime.now() + " server started successfully");
