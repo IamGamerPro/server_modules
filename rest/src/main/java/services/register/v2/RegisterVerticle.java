d8c4db93e01b4bd24c8486564c274c4d41960752
Sergey Kobets <sergey.kobec@gmail.com>
2016-04-10 21:29:35 +0300

    fix

--- rest/src/main/java/services/register/v2/RegisterVerticle.java
+++ rest/src/main/java/services/register/v2/RegisterVerticle.java
@@ -1,10 +1,10 @@
 package services.register.v2;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.http.HttpMethod;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.mongo.MongoClient;
 import io.vertx.ext.web.Router;
-import io.vertx.ext.web.handler.BodyHandler;
 import org.bson.types.ObjectId;
 import pro.iamgamer.core.security.PasswordUtils;
 import pro.iamgamer.routing.RouteOrchestrator;
@@ -15,68 +15,61 @@ import pro.iamgamer.routing.RouteOrchestrator;
  */
 public class RegisterVerticle extends AbstractVerticle {
     private MongoClient shared;
+
     @Override
     public void start() throws Exception {
-        JsonObject config = new JsonObject()
-                .put("connection_string", "mongodb://localhost:27017")
-                .put("db_name", "iamgamer")
-                .put("useObjectId", true);
-        shared = MongoClient.createShared(vertx, config);
-
+        JsonObject config = context.config();
+        JsonObject databaseConfig = config.getJsonObject("databaseConfig");
+        Integer port = config.getJsonObject("httServerConfig").getInteger("port");
+        shared = MongoClient.createShared(vertx, databaseConfig);
         RouteOrchestrator instance = RouteOrchestrator.getInstance(vertx, "/api");
 
         Router router = Router.router(vertx);
-
-        router.post().handler(BodyHandler.create());
         router.post().handler(routingContext -> {
-            JsonObject bodyAsJson = routingContext.getBodyAsJson();
-            UserCreateRequest obj = new UserCreateRequest(bodyAsJson);
-            routingContext.put("registerRequest", obj);
-            routingContext.next();
+            JsonObject registerRequest = routingContext.getBodyAsJson();
+            final String login = registerRequest.getString("login");
+            final String password = registerRequest.getString("password");
+            final String email = registerRequest.getString("email");
+            vertx.<byte[][]>executeBlocking(future -> {
+                try {
+                    System.out.println(Thread.currentThread().getName());
+                    final byte[] salt = PasswordUtils.randomSalt();
+                    final byte[] hash = PasswordUtils.hash(password.toCharArray(), salt);
+                    final byte[][] bytes = {hash, salt};
+                    future.complete(bytes);
+                } catch (Exception e) {
+                    future.fail(e);
+                }
+            }, false, calc -> {
+                System.out.println(Thread.currentThread().getName());
+                if (calc.succeeded()) {
+                    final byte[] hash = calc.result()[0];
+                    final byte[] salt = calc.result()[1];
+                    shared.createCollection("users",
+                            v -> {
+                                JsonObject document = new JsonObject();
+                                document.put("login", login);
+                                document.put("email", email);
+                                document.put("password", new JsonObject().put("$binary", hash));
+                                document.put("salt", new JsonObject().put("$binary", salt));
+                                shared.insert("users", document, generatedId -> {
+                                            if (generatedId.succeeded()) {
+                                                routingContext.response().setStatusCode(201);
+                                                routingContext.reroute(HttpMethod.POST, "/login");
+                                            } else {
+                                                routingContext.fail(generatedId.cause());
+                                            }
+                                        }
+                                );
+                            }
+                    );
+                }
+            });
         });
-        router.post().blockingHandler(routingContext -> {
-            UserCreateRequest registerRequest = routingContext.get("registerRequest");
-            final byte[] salt = PasswordUtils.randomSalt();
-            final byte[] hash = PasswordUtils.hash(registerRequest.password.toCharArray(), salt);
-            shared.createCollection("users",
-                    v -> {
-                        JsonObject document = new JsonObject();
-                        document.put("login", registerRequest.login);
-                        document.put("email", registerRequest.email);
-                        document.put("password", new JsonObject().put("$binary", hash));
-                        document.put("salt", new JsonObject().put("$binary", salt));
-                        shared.insert("users", document, generatedId -> {
-                                    if (generatedId.succeeded()) {
-                                        ObjectId objectId = new ObjectId(generatedId.result());
-                                        System.out.println(objectId.getDate());
-                                    }
-                                }
-                        );
-                    }
-            );
-            routingContext.next();
-        }, true);
-        router.post().handler(handler -> handler.response().setStatusCode(200).end());
+        instance.mountPublicSubRouter("/register/v1/", router);
+        vertx.createHttpServer().requestHandler(instance::accept).listen(port);
 
-        instance.mountPublicSubRouter("/register/v2/", router);
-
-        vertx.createHttpServer().requestHandler(instance::accept).listen(8080);
-    }
-
-    private class UserCreateRequest {
-        private final String login;
-        private final String password;
-        private final String email;
-
-        UserCreateRequest(JsonObject registerRequest) {
-            this.login = registerRequest.getString("login");
-            this.password = registerRequest.getString("password");
-            this.email = registerRequest.getString("email");
-        }
-    }
-
-    @Override
-    public void stop() throws Exception {
-        shared.close();
     }
 }
+
+
